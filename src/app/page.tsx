fc5df1a3b877d7f0276539c20a5a55c1a02a261c
Mitchell Bird <mitchell.bird@live.co.uk>
2025-12-17 02:52:08 +0000

    fixed nav bar order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ const sections = [
   { id: "about", label: "About" },
   { id: "experience", label: "Experience" },
   { id: "education", label: "Education" },
-  { id: "skills", label: "Skills" },
   { id: "projects", label: "Projects" },
+  { id: "skills", label: "Skills" },
   { id: "contact", label: "Contact" }
 ];
 
